34b98b87197392f769aacae0c620c302a59ed234
Akshita Dhariwal <25110cn108@cloud.com>
2026-06-01 15:16:51 +0530

    Added Factor of Number

---
+++ FactorOfNumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Factors of " << num << " are: ";
+
+    for (int i = 1;i <= num; i ++) {
+        if (num % 1 == 0) {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
