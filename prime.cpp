553cbba7a7a7987a1a762ba876b02ba3fb6f8fa2
Akshita Dhariwal <25110cn108@cloud.com>
2026-06-07 21:12:41 +0530

    Added prime program

---
+++ prime.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+int main() {
+    int num = 7;
+    if (isPrime(num)) 
+    cout << num << " is a Prime Number";
+    else
+    cout << num << " is not a Prime Number";
+    return 0;
+}
