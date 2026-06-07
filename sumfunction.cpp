23c440faedfce49522a71e41ba92db5d2f86a4b5
Akshita Dhariwal <25110cn108@cloud.com>
2026-06-07 20:58:25 +0530

    AQdded sum function program

---
+++ sumfunction.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int sum(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int result = sum(5, 10);
+    cout << "The sum is: " << result << endl;
+    return 0;
+}
