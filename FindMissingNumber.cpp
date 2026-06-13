3761a3f157848612d696618a849ca816c882444f
Akshita Dhariwal <25110cn108@cloud.com>
2026-06-13 21:08:34 +0530

    Added find missing number program

---
+++ FindMissingNumber.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter value of n; ";
+    cin >> n;
+int arr[n - 1];
+cout << "Enter " << n - 1 << " elements: ";
+for (int i = 0; i < n - 1; i++) {
+    cin >> arr[i];
+}
+int expectedSum = n * (n + 1) / 2;
+int actualSum = 0;
+
+for (int i = 0; i < n - 1; i++) {
+    actualSum += arr[i];
+}
+int missingNumber = expectedSum - actualSum;
+cout << "The missing number is: " << missingNumber << endl;
+return 0;
