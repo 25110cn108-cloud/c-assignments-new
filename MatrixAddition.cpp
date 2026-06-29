b4aa19b286afb7146febd62ffb0ae319d1deb8b0
Akshita Dhariwal <25110cn108@cloud.com>
2026-06-29 21:27:35 +0530

    Added matrix addition program

---
+++ MatrixAddition.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows, cols;
+    cout << "Enter number of rows and columns:";
+    cin >> rows >>cols;
+
+    int a[10][10], b[10][10], sum[10][10];
+    cout << "Enter elements of first matrix:\n";
+    for  (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    cout <<"Enter elements of second matrix;\n";
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> b[i][j];
+        }
+    }
+    cout << "Sum of matricsw:\n";
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            sum[i][j] = a[i][j] + b[i][j];
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
